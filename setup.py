67b90f67069f09e8216ec158838d605db42eeaab
Gerardo Salazar <gsalaz9800@gmail.com>
2020-06-11 17:31:07 -0700

    Adds deployment CI and changes setup.py parameters

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
+import pathlib
 from setuptools import setup
+
+CWD = pathlib.Path(__file__).parent
+README = (CWD / 'README.md').read_text()
 
 setup(
     name='quantconnect-lean-stubs',
-    version='0.1.0',
+    version='0.1.1',
+    description='Python type definitions for the Lean algorithmic trading engine',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/QuantConnect/quantconnect-lean-stubs',
     author='QuantConnect Corp.',
     author_email='support@quantconnect.com',
+    license='Apache 2.0',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License'
+    ],
     packages=[
         'QuantConnect',
         'QuantConnect.Data',
@@ -17,9 +29,6 @@ setup(
         'QuantConnect.Securities': ['py.typed', '*.pyi'],
         'QuantConnect.Algorithm': ['py.typed', '*.pyi'],
     },
-    url='https://www.quantconnect.com',
-    license='LICENSE.md',
-    description='Python type definitions for the Lean algorithmic trading engine',
     python_requires='>=3.6'
 )
 
